5bc6ac09aadbce708105bc4cc013d9da9cf0a50f
nasar <muhammadnasar012@gmail.com>
2023-11-28 15:56:57 +0500

    nasar 28nov

--- vpl/resources/views/customer_panel/billings/mastertalktime.blade.php
+++ vpl/resources/views/customer_panel/billings/mastertalktime.blade.php
@@ -91,13 +91,8 @@
 <br>
 
 <h4 style="font-weight:900;text-align:center">Talk Time Usage</h3>
-<<<<<<< Updated upstream
 <table class="table table-bordered table-responsive">
   <thead>
-=======
-<table class="table table-responsive">
-  <thead style="background-color:#0088cc;color:white;">
->>>>>>> Stashed changes
     <tr>
       <th scope="col">Phone No.</th>
       <th scope="col">Ring To</th>
