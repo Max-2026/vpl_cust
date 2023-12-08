61992ca1c0855e8bca03db17f49a704172e4a09a
Erdum <erdumadnan@gmail.com>
2023-12-08 16:11:14 +0500

    Update: dashboard page data populated dynamically

--- vpl/app/Http/Controllers/BuyNumberController.php
+++ vpl/app/Http/Controllers/BuyNumberController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Country;
+
 class BuyNumberController extends Controller
 {
     public function buy_number()
     {
-        return view('customer_panel.Buy_Numbers.buy_number');
+        $countries = Country::all();
+
+        return view('customer_panel.Buy_Numbers.buy_number', [
+            'countries' => $countries
+        ]);
     }
 
     public function buy_golden_number()
--- vpl/app/Http/Controllers/DashboardController.php
+++ vpl/app/Http/Controllers/DashboardController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('customer_panel.dashboard');
+        $user = Auth::user();
+
+        return view('customer_panel.dashboard', [
+            'user' => $user
+        ]);
     }
 }
