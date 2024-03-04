47731774044cfebca781abc610f327c927bbf666
Erdum <erdumadnan@gmail.com>
2024-03-05 01:30:56 +0500

    Feat(Vendors Integeration): Mechanism implemented for third party vendors API integeration

---
+++ vpl/app/VendorsAPI/VendorInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+interface VendorInterface
+{
+	public function fetch_numbers($country_dial_code, $prefix = null);
+	public function reserve($number);
+	public function unreserve($number);
+	public function purchase($number);
+	public function release($number);
+}
