e659f806fc4a257fede793051d6efcf94bddfd2f
Erdum <erdumadnan@gmail.com>
2024-03-23 23:17:39 +0500

    Feat(UI): Number type column and buy button added

--- vpl/resources/views/voice-numbers/index.blade.php
+++ vpl/resources/views/voice-numbers/index.blade.php
@@ -113,27 +113,27 @@
         <table class="min-w-full divide-y divide-gray-200">
           <thead class="bg-white">
             <tr>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              <!-- <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               	<input onchange="selectAllRows(event)" type="checkbox" class="focus:ring-cyan-600 h-4 w-4 text-cyan-700 border-gray-300 rounded">
-              </th>
+              </th> -->
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Number</th>
               <!-- <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Country</th> -->
               <!-- <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">City</th> -->
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Type</th>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Capabilities</th>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Rate ($ / minute)</th>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Monthly Price (fixed)</th>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Setup Charges</th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Toll Free</th>
-              <!-- <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap">Legal Requirements</th> -->
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap"></th>
             </tr>
           </thead>
           <tbody onclick="handleRowClick(event)">
 
             @foreach ($numbers as $number)
             <tr class="even:bg-white odd:bg-gray-50" data-number="{{ $number['number'] }}">
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+              <!-- <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                 <input type="checkbox" class="focus:ring-cyan-600 h-4 w-4 text-cyan-700 border-gray-300 rounded">
-              </td>
+              </td> -->
               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                 {{ $number['number'] }}
               </td>
@@ -143,6 +143,9 @@
               <!-- <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                 {{ $number['city'] }}
               </td> -->
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                Local
+              </td>
               <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                 Voice, SMS
               </td>
@@ -155,12 +158,9 @@
               <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                 ${{ $number['setup_charges'] }}
               </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                No
+              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                <a href="#" class="bg-cyan-600 text-white px-3 py-1 rounded hover:bg-cyan-700">Buy</a>
               </td>
-              <!-- <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                Yes
-              </td> -->
             </tr>
             @endforeach
 
