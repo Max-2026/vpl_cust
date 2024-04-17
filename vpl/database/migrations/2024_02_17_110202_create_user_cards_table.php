2ab29444fc0d8776000ab5b1dd718db81cf812bd
Erdum <erdumadnan@gmail.com>
2024-04-18 00:39:48 +0500

    Refactor(schema): Column renamed Individual indexes removed New composite unique index created

--- vpl/database/migrations/2024_02_17_110202_create_user_cards_table.php
+++ vpl/database/migrations/2024_02_17_110202_create_user_cards_table.php
@@ -14,14 +14,13 @@ return new class extends Migration
         Schema::create('user_cards', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('stripe_card_id')->unique();
-            $table->string('provider');
+            $table->string('stripe_card_id');
+            $table->string('brand');
             $table->string('expiry_month', 2);
             $table->string('expiry_year', 4);
             $table->string('last_digits', 4);
             $table->timestamps();
-            $table->index('user_id');
-            $table->index('stripe_card_id');
+            $table->unique(['user_id', 'stripe_card_id']);
         });
     }
 
