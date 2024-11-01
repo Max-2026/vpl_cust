0d9dd22976066fccab655b423e7d58c307230713
Erdum Adnan <erdumadnan@gmail.com>
2024-11-01 16:36:43 +0500

    Update handle_call_start.php

--- vpl/handle_call_start.php
+++ vpl/handle_call_start.php
@@ -17,7 +17,7 @@ $call_timestamp = date('Y-m-d H:i:s');
 $remote_ip = $agi->request['agi_peer'];
 $called_number = $agi->request['agi_extension'];
 
-$base_url = 'http://dialify.dgtlid.com';
+$base_url = 'https://portal.dialifi.com';
 $sip_secret = 'abcdefghijklmnopqrstuvwxyz';
 
 $url = $base_url . "/call-start/{$called_number}/{$sip_secret}?timestamp={$call_timestamp}&ip={$remote_ip}&caller={$caller_number}&call_id={$call_id}";
@@ -33,5 +33,5 @@ if (isset($data['sip_url'])) {
     $url = $base_url . "/call-end/{$call_id}/{$sip_secret}?timestamp={$call_end_timestamp}";
     $response = file_get_contents($url);
 } else {
-    $agi->exec("Playback", "number-destination-found-mono");
+    $agi->exec("Playback", "no-destination-found-mono");
 }
