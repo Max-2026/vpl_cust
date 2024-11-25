70763896be4bf47921443a2c1f179085e921ccee
Erdum <erdumadnan@gmail.com>
2024-11-25 22:51:45 +0500

    Refactor: Daily billing query improved
    Checking latest row

--- vpl/app/Services/BillingService.php
+++ vpl/app/Services/BillingService.php
@@ -3,42 +3,37 @@
 namespace App\Services;
 
 use App\Models\Number;
+use App\Models\NumberHistory;
 use App\Models\Invoice;
 
 class BillingService
 {
     public function generate_daily_invoices()
     {
-        Number::whereNotNull('current_user_id')
-            ->where('is_active', true)
-            ->withWhereHas('history', function ($query) {
-                $query->where('activity', 'purchased')
-                    ->whereDay('created_at', date('j'))
-                    ->whereNot(function ($query) {
-                        $query->whereMonth('created_at', date('n'))
-                            ->whereYear('created_at', date('Y'));
-                    })->limit(1);
-            })
-            ->whereDoesntHave('history', function ($query) {
-                $query->latest()
-                    ->limit(1)
-                    ->where('activity', 'release_requested')
-                    ->orWhere('activity', 'released');
-            })
-            ->whereDoesntHave('invoices', function ($query) {
-                $query->whereDate('created_at', date('Y-m-d'));
-            })
-            ->orderBy('current_user_id')
-            ->chunk(100, function ($numbers) {
-                $invoices = [];
+        $data = NumberHistory::withWhereHas('number', function ($query) {
+            $query->whereNotNull('current_user_id');
+        })
+        ->groupBy('number_id')
+        ->latest()
+        ->where('activity', 'purchased')
+        ->whereDay('created_at', date('j'))
+        ->whereNot(function ($query) {
+            $query->whereMonth('created_at', date('n'))
+                ->whereYear('created_at', date('Y'));
+        })
+        ->whereDoesntHave('number.invoices', function ($query) {
+            $query->whereDate('created_at', date('Y-m-d'));
+        })
+        ->chunk(100, function ($histories) {
+            $invoices = [];
 
-                foreach ($numbers as $number) {
-                    $invoices[] = $this->create_invoice($number);
-                }
+            foreach ($histories as $history) {
+                $invoices[] = $this->create_invoice($history->number);
+            }
 
-                Invoice::insert($invoices);
-            });
-    }
+            Invoice::insert($invoices);
+        });
+}
 
     private function create_invoice(Number $number)
     {
